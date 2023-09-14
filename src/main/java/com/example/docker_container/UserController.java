143b18846442c67c3d55340604de67d7f812331c
Vijitha Epa <vijithaepa@gmail.com>
2023-09-14 20:13:42 +1000

    integration 11

--- src/main/java/com/example/docker_container/UserController.java
+++ src/main/java/com/example/docker_container/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.Instant;
 
 @RestController
-@RequestMapping("/docker")
+@RequestMapping("/api/docker")
 public class UserController {
 
     @GetMapping("greeting")
