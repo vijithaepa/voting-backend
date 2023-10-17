8ec8b0e9da331436cdb7f561c9068b473f1e9547
Vijitha Epa <vijithaepa@gmail.com>
2023-10-17 21:18:56 +1100

    backend integration

--- src/main/java/com/example/docker_container/config/WebConfig.java
+++ src/main/java/com/example/docker_container/config/WebConfig.java
@@ -1,8 +1,12 @@
 package com.example.docker_container.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
