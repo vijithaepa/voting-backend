082a0a80b4ef2b0dcc84ee9e41302318a2d40178
Vijitha Epa <vijithaepa@gmail.com>
2023-10-17 21:13:20 +1100

    backend integration

---
+++ src/main/java/com/example/docker_container/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.example.docker_container.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowCredentials(false).maxAge(3600);
+    }
+}
