c73854cce43666a263ac2cb3e5323ecbdd4400cc
Vijitha Epa <vijithaepa@gmail.com>
2023-10-26 00:30:43 +1100

    java 17

---
+++ src/main/java/info/slnews/app/config/WebConfig.java
@@ -0,0 +1,17 @@
+package info.slnews.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowCredentials(false).maxAge(3600);
+    }
+}
