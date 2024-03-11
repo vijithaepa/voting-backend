2c772777c5d3a4014baca93ca2711e74cf5473fe
Vijitha Epa <vijithaepa@gmail.com>
2024-03-11 13:14:54 +1100

    sertificate

--- src/main/java/info/slnews/app/config/SecurityConfig.java
+++ src/main/java/info/slnews/app/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,4 +27,12 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .build();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(
+//                User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+//        return manager;
+//    }
 }
