772a419a719d739399dc2ad7787637d9e7e18746
Vijitha Epa <vijithaepa@gmail.com>
2024-03-19 20:43:29 +1100

    sec disable

--- src/main/java/info/slnews/app/config/SecurityConfig.java
+++ src/main/java/info/slnews/app/config/SecurityConfig.java
@@ -1,29 +1,29 @@
 package info.slnews.app.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-//                .csrf(Customizer.withDefaults())
-                .authorizeHttpRequests(auth -> {
-//                    auth.requestMatchers("/greeting/**").permitAll();
-                    auth.requestMatchers("/voting/**").permitAll();
-                    auth.anyRequest().permitAll();
-                })
-//                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
-                .build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+////                .csrf(Customizer.withDefaults())
+//                .authorizeHttpRequests(auth -> {
+////                    auth.requestMatchers("/greeting/**").permitAll();
+//                    auth.requestMatchers("/voting/**").permitAll();
+//                    auth.anyRequest().permitAll();
+//                })
+////                .httpBasic(Customizer.withDefaults())
+//                .formLogin(Customizer.withDefaults())
+//                .build();
+//    }
 
 //    @Bean
 //    public UserDetailsService userDetailsService() {
